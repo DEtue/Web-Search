eb022dc7ef58aac71a82afd9642fb4f61ee41e74
Dustin Etue <DEtue@users.noreply.github.com>
2015-11-08 16:20:32 -0600

    Remove irrelevant data

--- pyWeb.py
+++ pyWeb.py
@@ -1,25 +1,21 @@
 import webbrowser
 
-# Choose a search engine
-goog = "www.google.com/search?q="
-yahoo =  "search.yahoo.com/search?p="
-bing =  "www.bing.com/search?q="
-
-engines = [goog, yahoo, bing]
-
-#
+# open default browser and search using given parameters
 def searchFor(eng, query):
     webbrowser.open(eng + query, new=2)
 
+# get search engine if anything else besides the 3 choices 
 def getEngine(engine):
+    goog = "www.google.com/search?q="
+    yahoo =  "search.yahoo.com/search?p="
+    bing =  "www.bing.com/search?q="
+    duck = "https://duckduckgo.com/?q="
     if engine == "google":
         return goog
     elif engine == "yahoo":
         return yahoo
     elif engine == "bing":
         return bing
+    elif engine == "duck duck go":
+        return duck
 
-engine = input("Enter Search Engine: ").lower()
-query = input("Enter Search Query: ")
-
-searchFor(getEngine(engine), query)
